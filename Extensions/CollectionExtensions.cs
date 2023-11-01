1953995fdeab0f167ef97d3116103b7ebf2d01f8
Nikita Smolianitski <n.smolianitski@gmail.com>
2023-11-01 12:11:11 +0300

    Add singleton, collection GetRandom extension

---
+++ Extensions/CollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaboonAndCo.Extensions
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Select random element of collection.
+        /// </summary>
+        public static T GetRandom<T>(this IList<T> collection)
+        {
+            return collection.GetRandom(0);
+        }
+
+        /// <summary>
+        /// Select random element of collection with specified range. Uses size of collection when max is not declared.
+        /// </summary>
+        public static T GetRandom<T>(this IList<T> collection, int min, int max = 0)
+        {
+            var rng = Random.Range(min, max == 0 ? collection.Count : max);
+            return collection[rng];
+        }
+
+        /// <summary>
+        /// Select random element of collection with specified range. Can have out param with RNG index
+        /// </summary>
+        public static T GetRandom<T>(this IList<T> collection, out int rng, int min = 0, int max = 0)
+        {
+            rng = Random.Range(min, max == 0 ? collection.Count : max);
+            return collection[rng];
+        }
+    }
+}
---
+++ Patterns/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace BaboonAndCo.Patterns
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; protected set; }
+
+        private void Awake()
+        {
+            foreach (var instance in FindObjectsOfType(typeof(T), false))
+            {
+                if (instance != this)
+                {
+                    var go = (instance as T)!.gameObject;
+                    WarnAboutDuplicate(go);
+                    DestroyImmediate(go);
+                }
+            }
+
+            Instance = this as T;
+        }
+
+        private void OnApplicationQuit()
+        {
+            Instance = null;
+        }
+
+        private void WarnAboutDuplicate(GameObject instance)
+        {
+            Debug.LogError
+            (
+            "Found Singleton duplicate in scene hierarchy. " +
+                $"Type of: <color=red>{typeof(T).Name}</color> " +
+                $"and GO name: <color=red>{instance.name}</color>!"
+            );
+        }
+    }
+}
