b5ae77d36ddab63c2c45512e38a91d510039b0a6
Nikita Smolianitski <n.smolianitski@gmail.com>
2023-11-01 23:57:45 +0300

    Add transform.AlignWithParent extension method

---
+++ Extensions/TransformExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace BaboonAndCo.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void AlignWithParent(this Transform transform)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = transform.parent.localRotation;
+        }
+    }
+}
