6f65339a9b305fda8cb6d3fb658d21c557739b20
Nikita Smolianitski <n.smolianitski@gmail.com>
2023-11-01 23:06:35 +0300

    Add GetUnitRelativeDirection

---
+++ Utils/TransformUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace BaboonAndCo.Utils
+{
+    public static class TransformUtils
+    {
+        public static Vector3 GetUnitRelativeDirection(Transform transform, in Vector3 direction)
+        {
+            return (transform.right * direction.x)
+                   + (transform.forward * direction.z);
+        }
+    }
+}
